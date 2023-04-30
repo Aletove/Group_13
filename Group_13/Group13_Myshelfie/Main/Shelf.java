ee1586626c9ed471beaeefe08e3b9e3d0ac8981c
HamzaAnes <h.anes@studenti.unibg.it>
2023-04-30 19:26:34 +0200

    Shelf's class appdated

--- Group_13/Group13_Myshelfie/Main/Shelf.java
+++ Group_13/Group13_Myshelfie/Main/Shelf.java
@@ -1,5 +1,21 @@
 package Main;
 
 public class Shelf {
-
+	char matrix[][];
+	
+	public Shelf() {
+		matrix=new char[6][5];
+	}
+	
+	public int NumberOfEmptyCells(){
+		int count=0;
+		for(int i=0;i<6;i++) {
+			for (int j=0;j<5;j++) {
+				if(matrix[i][j]==0) {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
 }
