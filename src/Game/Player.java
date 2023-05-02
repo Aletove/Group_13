e451d6152f16d331d4f6b307ae83e8d3688693bb
Aletove <127764761+Aletove@users.noreply.github.com>
2023-05-02 21:48:39 +0200

    Revert "Player v 0"

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -31,17 +31,12 @@ public class Player {
 		this.isFirst = newIsFirst;
 	}
 
-	public int calculateScore(boolean isFirstToFinish) {
+	public int calculateScore() {
 		// TODO: waiting for others
 		int total=0;
 		total+=calculatePObjectiveScore();
 		total+=calculateCommonScore();
-		total+=calculateAdjacentScore();
-		return total+(isFirstToFinish?1:0);
-	}
-	private int calculateAdjacentScore() {
-		//for shelf.explore() => girare dentro lo switch e calcolare un risultato parziale
-		return 1;
+		return total;
 	}
 	private int calculateCommonScore() {
 		int total=0;
@@ -49,33 +44,30 @@ public class Player {
 	}
 	private int calculatePObjectiveScore() {
 		int total=0;
-		int rightTiles=0;
-		for( int i=0 ; i<this.pObjective.goalTiles.length;i++) {
-			/*if(this.shelf.isTileFilled(this.pObjective.goalTiles[i])) {
-			  	rightTiles++;
-			}*/
-		}
-		switch(rightTiles) {
-		case 0:
-			total+=1;
-			break;
-		case 1:
-			total+=2;
-			break;
-		case 2:
-			total+=4;
-			break;
-		case 3:
-			total+=6;
-			break;
-		case 4:
-			total+=9;
-			break;
-		case 5:
-			total+=12;
-			break;
-	}
-
+		/*for( int i=0 ; i<this.pObjective.goalTiles.length;i++) {
+			/*if(this.shelf.isTileFilled(this.pObjective.goalTiles[i].x,this.pObjective.goalTiles[i].y)) {
+				switch(i) {
+					case 0:
+						total+=1;
+						break;
+					case 1:
+						total+=2;
+						break;
+					case 2:
+						total+=4;
+						break;
+					case 3:
+						total+=6;
+						break;
+					case 4:
+						total+=9;
+						break;
+					case 5:
+						total+=12;
+						break;
+				}
+			}
+		}*/
 		return total;
 	}
 	
