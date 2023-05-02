37b5bc0ecd00d42215815e4f1dab1e516de83e53
Mariie49 <m.lamara@studenti.unibg.it>
2023-05-02 18:57:32 +0200

    basic skeleton for tile

--- src/Game/Tile.java
+++ src/Game/Tile.java
@@ -1,5 +1,12 @@
 package Game;
 
 public class Tile {
-
+	public int x;
+	public int y;
+	public Color color;
+	public Tile(int newX, int newY, Color newColor) {
+		this.x=newX;
+		this.y=newY;
+		this.color=newColor;
+	}
 }
