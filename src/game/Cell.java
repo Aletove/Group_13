36a37fb8187c18b68dc6309761f28a7ecb847334
DavideCarissoni <126593384+DavideCarissoni@users.noreply.github.com>
2023-05-09 21:03:19 +0200

    new board class, fixed cell class

--- src/game/Cell.java
+++ src/game/Cell.java
@@ -1,5 +1,38 @@
-package game;
+package Game;
 
 public class Cell {
-
+	
+	public int x, y, index;
+	//index: 0=unused, 1=2 players, 2=3 players, 3=4 players 
+	
+	public boolean state;
+	
+	Cell cells = new Cell(x, y, state, index);
+	public Cell(int x, int y, boolean state, int index) {
+		this.x=x;
+		this.y=y;
+		this.state=state;
+		this.index=index;
+	}
+	
+	public boolean isFree(){
+		
+		if (this.cells == null && state == true) {
+			return true;
+		}
+		else {
+			return false;
+		}
+			
+	}
+	
+	public void setIndex(int index) {
+		this.index = index;
+		
+	}
+	
+	public void setState(boolean state) {
+		this.state= state;
+	}
+	
 }
