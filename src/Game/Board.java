24437793c813b38aad3e693821473a242bf57c9d
DavideCarissoni <126593384+DavideCarissoni@users.noreply.github.com>
2023-05-11 12:28:31 +0200

    updeted board

--- src/Game/Board.java
+++ src/Game/Board.java
@@ -1,161 +1,61 @@
 package Game;
-import Game.Cell;
-import Game.Bag;
-import Game.Tile;
+
 public class Board {
 
 	private int SIZE=9;
+	private int nPlayers;
+	private int[][] mold =  {
+			{0, 0, 0, 3, 4, 0, 0, 0, 0},
+			{0, 0, 0, 2, 2, 4, 0, 0, 0},
+			{0, 0, 3, 2, 2, 2, 3, 0, 0},
+			{0, 4, 2, 2, 2, 2, 2, 2, 3},
+			{4, 2, 2, 2, 2, 2, 2, 2, 4},
+			{3, 2, 2, 2, 2, 2, 2, 4, 0},
+			{0, 0, 3, 2, 2, 2, 3, 0, 0},
+			{0, 0, 0, 2, 2, 2, 0, 0, 0},
+			{0, 0, 0, 0, 4, 3, 0, 0, 0},
+	};
+	private	Tile[][] matrix;
+	private Bag boardBag = new Bag();
 
-	private Cell[][] matrix = new Cell[SIZE][SIZE];
-
-	private int x, y, index, nPlayers;
-	private int[] picAreaRows[];
-	private int[] pickAreaColumns[];
-	
-	Bag bag = new Bag();
-
-	private boolean state;
-
-	public void setPlayers(int nPlayers) {
-		this.nPlayers = nPlayers;
-	}
-	
-	private Board() {
-		for(int i=0; i<SIZE; i++) {
-			for(int j=0; j<SIZE; j++) {
-				matrix[i][j] = new Cell(i, j, false, index);
-				matrix[i][j] = null;
-			}
-		}	
-
-		//celle non usabili:
-		matrix[0][0].setIndex(0);
-		matrix[0][1].setIndex(0);
-		matrix[0][2].setIndex(0);
-		matrix[0][5].setIndex(0);
-		matrix[0][6].setIndex(0);
-		matrix[0][7].setIndex(0);
-		matrix[0][8].setIndex(0);
-		matrix[1][0].setIndex(0);
-		matrix[1][1].setIndex(0);
-		matrix[1][2].setIndex(0);
-		matrix[1][6].setIndex(0);
-		matrix[1][7].setIndex(0);
-		matrix[1][8].setIndex(0);
-		matrix[2][0].setIndex(0);
-		matrix[2][1].setIndex(0);
-		matrix[2][2].setIndex(0);
-		matrix[2][3].setIndex(0);
-		matrix[3][0].setIndex(0);
-		matrix[5][8].setIndex(0);
-		matrix[6][0].setIndex(0);
-		matrix[6][1].setIndex(0);
-		matrix[6][7].setIndex(0);
-		matrix[6][8].setIndex(0);
-		matrix[7][0].setIndex(0);
-		matrix[7][1].setIndex(0);
-		matrix[7][2].setIndex(0);
-		matrix[7][6].setIndex(0);
-		matrix[7][7].setIndex(0);
-		matrix[7][8].setIndex(0);
-		matrix[8][0].setIndex(0);
-		matrix[8][1].setIndex(0);
-		matrix[8][2].setIndex(0);
-		matrix[8][3].setIndex(0);
-		matrix[8][6].setIndex(0);
-		matrix[8][7].setIndex(0);
-		matrix[8][8].setIndex(0);
-
-		//celle per 2 giocatori: 
-		matrix[1][3].setIndex(1);
-		matrix[1][4].setIndex(1);
-		matrix[2][3].setIndex(1);
-		matrix[2][4].setIndex(1);
-		matrix[2][5].setIndex(1);
-		matrix[3][2].setIndex(1);
-		matrix[3][3].setIndex(1);
-		matrix[3][4].setIndex(1);
-		matrix[3][5].setIndex(1);
-		matrix[3][6].setIndex(1);
-		matrix[3][7].setIndex(1);
-		matrix[4][1].setIndex(1);
-		matrix[4][2].setIndex(1);
-		matrix[4][3].setIndex(1);
-		matrix[4][4].setIndex(1);
-		matrix[4][5].setIndex(1);
-		matrix[4][6].setIndex(1);
-		matrix[4][7].setIndex(1);
-		matrix[5][1].setIndex(1);
-		matrix[5][2].setIndex(1);
-		matrix[5][3].setIndex(1);
-		matrix[5][4].setIndex(1);
-		matrix[5][5].setIndex(1);
-		matrix[5][6].setIndex(1);
-		matrix[6][3].setIndex(1);
-		matrix[6][4].setIndex(1);
-		matrix[6][5].setIndex(1);
-		matrix[7][4].setIndex(1);
-		matrix[7][5].setIndex(1);
-
-		//celle per 3 giocatori
-		matrix[0][3].setIndex(2);
-		matrix[2][2].setIndex(2);
-		matrix[2][6].setIndex(2);
-		matrix[3][8].setIndex(2);
-		matrix[5][0].setIndex(2);
-		matrix[6][2].setIndex(2);
-		matrix[6][6].setIndex(2);
-		matrix[8][5].setIndex(2);
-
-		//celle per 4 giocatori:
-
-		matrix[0][4].setIndex(3);
-		matrix[1][5].setIndex(3);
-		matrix[3][1].setIndex(3);
-		matrix[4][0].setIndex(3);
-		matrix[4][8].setIndex(3);
-		matrix[5][7].setIndex(3);
-		matrix[7][3].setIndex(3);
-		matrix[8][4].setIndex(3);
+	public Board(int nPlayer) {
 
 		for(int i=0; i<SIZE; i++) {
 			for(int j=0; j<SIZE; j++) {
-				if(nPlayers == 2) {
-					if(matrix[i][j].index == 1) {
-						matrix[i][j].setState(true);;
-					}
-				} else if(nPlayers == 3){
-					if(matrix[i][j].index == 2 || matrix[i][j].index == 1) {
-						matrix[i][j].setState(true);;
-					}
-					
-				} else if(nPlayers == 4){
-					if(matrix[i][j].index == 3 || matrix[i][j].index == 2 || matrix[i][j].index == 1) {
-						matrix[i][j].setState(true);;
-					}
-					
+				if(mold[i][j]==nPlayers) {
+					matrix[i][j] = Tile.EMPTY;
 				}
-
 			}
 		}	
-
-
 	}
 
+	public boolean isPickable(int x, int y) {
+		boolean temp = false;
+		if(matrix[x-1][y] == null  || matrix[x][y-1] == null  || matrix[x+1][y] == null  || matrix[x][y+1] == null) {
+			temp = true;
+		}
 
+		return temp;
+	}	
 
-
-	public void pickTiles(int picAreaRows[], int pickAreaColumns[]) {
-
+	public void pickTiles(int pickAreaRows[], int pickAreaColumns[]) {
+		for(int i=0; i<pickAreaRows.length; i++) {
+			for(int j=0; j<pickAreaColumns.length; j++) {
+				if(!isPickable(i, j)) {
+					System.out.println("Scelta non valida");
+				}
+			}
+		}
 
 	}
-	public void fillBoard(Bag bag) {
+	public void fillBoard(Bag boardBag) {
 		for(int i=0; i<SIZE; i++) {
 			for(int j=0; j<SIZE; j++) {
-				
-		}	
+				if(matrix[i][j] == Tile.EMPTY) {
+					matrix[i][j] = boardBag.getTile();
+				}
+			}
+		}
 	}
-	public void isPickable() {
 
-	}
 }
