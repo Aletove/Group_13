d4f6df17a2e734f89ab80a71cacb5b8a8a86610c
HamzaAnes <h.anes@studenti.unibg.it>
2023-05-11 10:09:49 +0200

    Update shelf.java

--- src/Game/Shelf.java
+++ src/Game/Shelf.java
@@ -12,7 +12,17 @@ public class Shelf {
 	public Shelf() {
 		matrix=new Tile[rows][columns];
 	}
-	
+	/**
+	 * 
+	 * @return the matrix of shelf
+	 */
+	public Tile[][] getShelf() {
+		return matrix;
+	}
+	/**
+	 * 
+	 * @return the number of empty cells on the shelf
+	 */
 	public int NumberOfEmptyCells(){
 		int count=0;
 		for(int i=0;i<matrix.length;i++) {
@@ -24,6 +34,11 @@ public class Shelf {
 		}
 		return count;
 	}
+	/**
+	 * 
+	 * @param column
+	 * @return the number of empty cells on the single column
+	 */
 	public int NumberOfEmptyCellsOnColumn(int column){
 	    int count=0;
 	    for (int i=0;i<matrix.length;i++){
@@ -33,7 +48,10 @@ public class Shelf {
 	    }
 	    return count;
 	}
-	
+	/**
+	 * 
+	 * @return true if the shelf is full
+	 */
 	public Boolean isFull() {
 		if(this.NumberOfEmptyCells()==0) {
 			return true;
@@ -42,10 +60,15 @@ public class Shelf {
 			return false;
 		}
 	}
-	
-	public Boolean isPlaceable(int column) {
-		if(this.NumberOfEmptyCellsOnColumn(column)==0) {
-			return false;
+	/**
+	 * 
+	 * @param column
+	 * @param noftiles
+	 * @return true if there is enough empty cells on the column for the number of tiles
+	 */
+	public Boolean isPlaceable(int column,int noftiles) {
+		if(this.NumberOfEmptyCellsOnColumn(column)<=noftiles) {
+			return true;
 		}else {
 			return false;
 		}
@@ -55,6 +78,9 @@ public class Shelf {
 		
 	}
 	
+	/**
+	 * print the shelf
+	 */
 	public String toString(){
 	    String s="";
 	    for(int i=0;i<matrix.length;i++) {
@@ -69,8 +95,76 @@ public class Shelf {
 		}
 		return s;
 	}
+	public int adjacentTilescount(Tile t){
+        int cont=0;
+        for(int i=0;i<6;i++){
+            for(int j=0;j<5;j++){
+                if(this.matrix[i][j].equals(t)){
+                    if(i==0&&j==0){
+                        if(this.matrix[i][j].equals(this.matrix[i][j+1]) || this.matrix[i][j].equals(this.matrix[i+1][j]))
+                            cont++;
+                    }
+                    if(i==0&&j==columns-1){
+                        if(this.matrix[i][j].equals(this.matrix[i][j-1]) || this.matrix[i][j].equals(this.matrix[i+1][j]))
+                            cont++;
+                    }
+                    if(i==rows-1&&j==0){
+                        if(this.matrix[i][j].equals(this.matrix[i-1][j]) || this.matrix[i][j].equals(this.matrix[i][j+1]))
+                            cont++;
+                    }
+                    if(i==rows-1&&j==columns-1){
+                        if(this.matrix[i][j].equals(this.matrix[i][j-1]) || this.matrix[i][j].equals(this.matrix[i-1][j]))
+                            cont++;
+                    }
+                    if(i==0&&j>0&&j<columns-1){
+                        if(this.matrix[i][j].equals(this.matrix[i+1][j]) || this.matrix[i][j].equals(this.matrix[i][j-1]) || this.matrix[i][j].equals(this.matrix[i][j+1]))
+                            cont++;
+                    }
+                    if(i==rows-1&&j>0&&j<columns-1){
+                        if(this.matrix[i][j].equals(this.matrix[i-1][j]) || this.matrix[i][j].equals(this.matrix[i][j-1]) || this.matrix[i][j].equals(this.matrix[i][j+1]))
+                            cont++;
+                    }
+                    if(j==0&&i>0&&i<rows-1){
+                        if(this.matrix[i][j].equals(this.matrix[i][j+1]) || this.matrix[i][j].equals(this.matrix[i-1][j]) || this.matrix[i][j].equals(this.matrix[i+1][j]))
+                            cont++;
+                    }
+                    if(j==columns-1&&i>0&&i<rows-1){
+                        if(this.matrix[i][j].equals(this.matrix[i][j-1]) || this.matrix[i][j].equals(this.matrix[i-1][j]) || this.matrix[i][j].equals(this.matrix[i+1][j]))
+                            cont++;
+                    }
+                    if(i>0&&j>0&&i<rows-1&&j<columns-1){
+                        if(this.matrix[i][j].equals(this.matrix[i][j+1]) ||this.matrix[i][j].equals(this.matrix[i][j-1]) || this.matrix[i][j].equals(this.matrix[i+1][j]) ||this.matrix[i][j].equals(this.matrix[i-1][j]))
+                            cont++;
+                    }
+                }
+            }
+        }
+        return cont;
+    }
+    public int adjacentTilesScore(){
+        int count=0;
+        Tile t[]=new Tile[6];;
+        c[0]='r';
+        c[1]='c';
+        c[2]='t';
+        c[3]='g';
+        c[4]='l';
+        c[5]='b';
+        for(int i=0;i<6;i++){
+            if(adjacentTilescount(t[i])==3){
+                count+=2;
+            }
+            if(adjacentTilescount(t[i])==4){
+                count+=3;
+            }
+            if(adjacentTilescount(t[i])==5){
+                count+=5;
+            }
+            if(adjacentTilescount(t[i])>=6){
+                count+=8;
+            }
+        }
+        return count;
+    }
 
-	public Tile[][] getShelf() {
-		return matrix;
-	}
 }
