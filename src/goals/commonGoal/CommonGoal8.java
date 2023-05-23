573242ed1e496f13392fb30b96095ccbf8a39d9e
HamzaAnes <127412633+HamzaAnes@users.noreply.github.com>
2023-05-23 22:01:33 +0200

    Update CommonGoal8.java

--- src/goals/commonGoal/CommonGoal8.java
+++ src/goals/commonGoal/CommonGoal8.java
@@ -3,6 +3,9 @@ import game.*;
 import goals.*;
 import java.util.*;
 
+import Game.Player;
+import Game.Tile;
+
 public class CommonGoal8 extends Goal{
 	private ArrayList<String> hasCompletedID;
 
@@ -18,19 +21,31 @@ public class CommonGoal8 extends Goal{
 		
 	}
 	
+	
 	@Override
 	public int isCompleted(Player currentPlayer) {
 		Tile[][] shelf;
 		shelf = currentPlayer.getShelf();
-
+		int cont=0; //a counter for the number of column that contain at most three different type of tile
 		
-		for(int i = shelf.length; i > 0; i--) {
-			for(int j = shelf[i].length; j > 0; i--) {
-				
+		for(int i = shelf[0].length; i > 0; i--) {
+			Set<Tile> differetsTile = new HashSet<>(); //use the fact that the Set can't store duplicate values to calculate the number of the different tile contained in the column
+			for(int j = shelf.length; j > 0; i--) {
+				differetsTile.add(shelf[j][i]);
 			}
+			if (differetsTile.size() <= 3) {
+	            cont++;
+	        }else {
+	        	cont++;;
+	        }
+			differetsTile.clear();
+		}
+		if(cont>=3) {
+			return super.pointsMethod();
 		}
 		return 0;
 	}
+	
 	
 
 }
