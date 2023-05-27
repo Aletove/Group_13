0c5fbc8b83d8ed5f8f88f3fb7b84cceb11e2f9bf
giuseppeluisi <127615371+giuseppeluisi@users.noreply.github.com>
2023-05-27 18:51:17 +0200

    Update CommonGoal5.java

--- src/goals/commonGoal/CommonGoal5.java
+++ src/goals/commonGoal/CommonGoal5.java
@@ -4,8 +4,7 @@ import goals.*;
 import java.util.*;
 
 public class CommonGoal5 extends Goal{
-	private ArrayList<String> hasCompletedID;
-
+	private ArrayList<Integer> hasCompletedID;
 	
 	
 	/**The constructor generates the array list points,
@@ -14,20 +13,49 @@ public class CommonGoal5 extends Goal{
 	 */
 	public CommonGoal5(int nPlayers) {
 		super(nPlayers);
-		hasCompletedID = new ArrayList<String>();
+		hasCompletedID = new ArrayList<Integer>();
 		
 	}
 	
 	@Override
 	public int isCompleted(Player currentPlayer) {
 		Tile[][] shelf;
+		HashSet<Tile> tileTypes;
+		int rows, cols, validCols = 0;
+		boolean isValidCol;
+		Tile currentTile;
 		shelf = currentPlayer.getShelf();
+		tileTypes = new HashSet<>();
+		cols = shelf.length; //controlla
+		rows = shelf[0].length;//controlla
+		isValidCol = true;
 
-		
-		for(int i = shelf.length; i > 0; i--) {
-			for(int j = shelf[i].length; j > 0; i--) {
+		//check each column
+		for(int i = 0; i < cols; i++) {
+			tileTypes = new HashSet<>();
+			isValidCol = true;
+			//check each row in the current column
+			for(int j = 0; j < rows && isValidCol; i++) {
+				currenTile = shelf[i][j];
+				if(!currentTile.equals(Tile.EMPTY) || tileTypes.size()<2) {//If the row has one or more empty tiles, is not valid.
+					tileTypes.add(currentTile);
+				}else {
+					isValidCol = false;
+				}
 				
 			}
+			if(isValidCol) {
+				validCols++;
+			}
+			
+		}
+		
+		//check if the goal was reached
+		if(validCols > 5) {
+			if(super.notCompletedYet(currentPlayer, hasCompletedID)) {
+				return super.pointsMethod();
+			}
+			System.out.println("Goal reached");
 		}
 		return 0;
 	}
