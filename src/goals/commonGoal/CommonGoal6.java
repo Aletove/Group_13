0205991b230ff4dbf920f37bfdf373b9d32cbccf
0xMattia <126601545+0xMattia@users.noreply.github.com>
2023-05-28 03:20:38 +0200

    final version of commongoal6 and commongoal9

--- src/goals/commonGoal/CommonGoal6.java
+++ src/goals/commonGoal/CommonGoal6.java
@@ -4,7 +4,12 @@ import goals.*;
 import java.util.*;
 
 public class CommonGoal6 extends Goal{
-	private ArrayList<String> hasCompletedID;
+	private ArrayList<Integer> hasCompletedID;
+	private static final int[][] SQUAREDIRECTIONS = {// Directions: down, right, diagonal right. top is excluded
+				{0, 1},
+				{1, 0},
+				{1, 1}
+				};
 
 	
 	
@@ -14,23 +19,82 @@ public class CommonGoal6 extends Goal{
 	 */
 	public CommonGoal6(int nPlayers) {
 		super(nPlayers);
-		hasCompletedID = new ArrayList<String>();
+		hasCompletedID = new ArrayList<Integer>();
 		
 	}
 	
 	@Override
 	public int isCompleted(Player currentPlayer) {
 		Tile[][] shelf;
+		int rows, cols;
+		Set<String> foundGroups;
 		shelf = currentPlayer.getShelf();
-
-		
-		for(int i = shelf.length; i > 0; i--) {
-			for(int j = shelf[i].length; j > 0; i--) {
-				
-			}
+		foundGroups = new HashSet<>();
+		rows = shelf.length;
+		cols = shelf[0].length;
+		 // Iterate through all the cells of the shelf
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+               
+            	Tile targetType = shelf[i][j]; // Tile type that we are looking for
+                    
+            	Set<String> currentGroup = new HashSet<>(); // Current group of Tiles
+                    
+            	// Executes findAdjacent to find the group of adjacent tiles of the same type forming a 2x2 square
+                findAdjacent(shelf, i, j, targetType, currentGroup);
+                // If the group is formed by four tiles, we store it in the set foundGroups
+                if (currentGroup.size() == 4) {
+                	foundGroups.addAll(currentGroup);
+                   	System.out.println("Group found: " + currentGroup);
+                }
+            }
+        }
+        
+        if(foundGroups.size() >= 2) {
+        	if(super.notCompletedYet(currentPlayer, hasCompletedID)) {
+        		return super.pointsMethod();
+        	}
+        }
+        
+        return 0;
+    }
+	
+	
+	/**
+	 * this method uses an implementation of Depth - first research to find groups of adjacent tiles
+	 * @param grid the shelf used in the method
+	 * @param row x coordinate of the tile
+	 * @param col y coordinate of the tile
+	 * @param targetType (e.g. Tile.CATS)
+	 * @param currentGroup Set of strings containing the current group of tiles
+	 */
+	private void findAdjacent(Tile[][] grid, int row, int col, Tile targetType, Set<String> currentGroup) {
+		int rows = grid.length;
+		int cols = grid[0].length;
+	        
+		// Boundary and tile type check
+		if (row < 0 || row >= rows || col < 0 || col >= cols || !grid[row][col].equals(targetType) || currentGroup.size() > 4) {
+			return;
 		}
-		return 0;
+	        
+		String position = row + "-" + col; //the format of the position is the same in all the groups
+	        
+		// Check if the tile has already been found
+		if (currentGroup.contains(position)) {
+			return;
+		}
+		
+		currentGroup.add(position);
+	        
+		// Explore adjacent tiles
+		for (int[] direction : SQUAREDIRECTIONS) {
+			int newRow = row + direction[0];
+			int newCol = col + direction[1];
+	            
+			findAdjacent(grid, newRow, newCol, targetType, currentGroup);
+		}
 	}
+
 	
 
 }
--- src/goals/commonGoal/CommonGoal9.java
+++ src/goals/commonGoal/CommonGoal9.java
@@ -1,36 +1,66 @@
-package goals.commonGoal;
+package Objectives.commonObj;
 import game.*;
 import goals.*;
 import java.util.*;
 
-public class CommonGoal9 extends Goal{
-	private ArrayList<String> hasCompletedID;
+import Game.Tile;
 
-	
-	
+public class CommonGoal9 extends Goal{
+	private ArrayList<Integer> hasCompletedID;
+	private static final int [][] XDIRECTIONS = {//directions to use, assuming that the element is in the center of the shape.
+					{-1,-1},
+					{-1,1},
+					{1,-1},
+					{1,1}
+		};
+  
+  
 	/**The constructor generates the array list points,
 	 * based on the number of players.
 	 * @param nPlayers
 	 */
 	public CommonGoal9(int nPlayers) {
 		super(nPlayers);
-		hasCompletedID = new ArrayList<String>();
-		
+		hasCompletedID = new ArrayList<Integer>();
+    
 	}
-	
+  
 	@Override
-	public int isCompleted(Player currentPlayer) {
+  	public int isCompleted(Player currentPlayer) {
 		Tile[][] shelf;
+		int rows, cols;
 		shelf = currentPlayer.getShelf();
-
+		rows = shelf.length;
+		cols = shelf[0].length;
+		int cont = 0;
 		
-		for(int i = shelf.length; i > 0; i--) {
-			for(int j = shelf[i].length; j > 0; i--) {
-				
-			}
-		}
-		return 0;
-	}
-	
-
+		 // Iterate through all the cells of the shelf
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+               
+            	Tile targetType = shelf[i][j]; // Tile type that we are looking for
+            	
+            	for (int[] direction : XDIRECTIONS) {
+            			int newRow = i + direction[0];
+            			int newCol = j + direction[1];
+            			if (newRow >= 0 && newRow < 6 && newCol >= 0 && newCol < 5) {
+            				if(targetType.equals(shelf[newRow][newCol])) {
+            					cont++;
+            					//checking if there are 5 Tiles forming an X
+            					if(cont == 5 && super.notCompletedYet(currentPlayer, hasCompletedID)) {
+            						return super.pointsMethod();
+            					}
+            				}
+            			}
+            	}
+            	
+            	//cont to 0 for new check 
+            	cont = 0;
+            	
+                }
+            }
+        }
+        
+        return 0;
+    }
 }
