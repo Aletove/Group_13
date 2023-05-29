9f3491cf3536d4a2f0b036030d50130285547e18
giuseppeluisi <127615371+giuseppeluisi@users.noreply.github.com>
2023-05-29 11:23:31 +0200

    Update CommonGoal7.java

--- src/goals/commonGoal/CommonGoal7.java
+++ src/goals/commonGoal/CommonGoal7.java
@@ -22,30 +22,30 @@ public class CommonGoal7 extends Goal {
 		int rows, cols, validRows = 0;
 		boolean isValidRow;
 		Tile currentTile;
-		shelf = currentPlayer.getshelf();
-		rows = shelf.lenght;
+		shelf = currentPlayer.getShelf();
+		rows = shelf.length;
 		cols = shelf[0].length;
 		isValidRow = true;
 		
 	//check each row of the shelf
 	for(int i = 0; i < rows; i++) {
-		tileTypes = new HashSet<>();
+		tileTypes = new HashSet<Tile>();
 		isValidRow = true;
 		//check each column in the current row
 		for(int j = 0; j < cols && isValidRow; j++) {
 			currentTile = shelf[i][j];
-			if(!currentTile.equals(Tile.EMPTY) || tileTypes.size()<2) {//If the row has one or more empty tiles, is not valid.
+			if(!currentTile.equals(Tile.EMPTY) || tileTypes.size()<6) {//If the row has one or more empty tiles, is not valid.
 				tileTypes.add(currentTile);
 			}else {
 				isValidRow = false;
 			}
 		}
-		if(isValidRow) {
+		if(isValidRow && tileTypes.size()==5) {
 			validRows++;
 		}
 	}
 	//check if the goal was reached
-	if(validRows > 5) {
+	if(validRows > 1) {
 		if(super.notCompletedYet(currentPlayer, hasCompletedID)) {
 			return super.pointsMethod();
 		}
